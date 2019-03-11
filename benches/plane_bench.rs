adc02903c17244b09bd03c7259a57a221405d1a2
Max Orok <morok@mevex.com>
2019-03-11 16:15:28 -0400

    added xy vs xz benchmarks -> roughly 70ns difference in creation, should be OK for most purposes

--- benches/plane_bench.rs
+++ benches/plane_bench.rs
@@ -2,25 +2,37 @@ extern crate criterion;
 use criterion::{Criterion, criterion_group, criterion_main};
 
 extern crate geo;
+
 // --
 
 fn bench_memoized_inverse(c: &mut Criterion) {
+    //c.bench_function("inverse global_to_local ", |b| b.iter(||
 }
 
 fn bench_eager_inverse(c: &mut Criterion) {
+
 }
 
 fn bench_make_xy_plane(c: &mut Criterion) {
-    //let xy = geo::Plane::xy_plane(geo::RectBounds::new(2.3, 100.0));
+    let bounds = geo::RectBounds::new(100.0, 100.0);
+
+    c.bench_function("make xy plane",
+        move |b| b.iter(|| geo::xy_plane(&bounds))
+    );
 }
 
 fn bench_make_yz_plane(c: &mut Criterion) {
+    let bounds = geo::RectBounds::new(100.0, 100.0);
 
+    c.bench_function("make xz plane",
+        move |b| b.iter(|| geo::xz_plane(&bounds))
+    );
 }
 
 criterion_group!(benches, bench_memoized_inverse,
                           bench_eager_inverse,
                           bench_make_xy_plane,
-                          bench_make_yz_plane);
+                          bench_make_yz_plane,
+                );
 
 criterion_main!(benches);
