7ffdb244cc4e11ee7666e03227b65320ede568bd
Max Orok <morok@mevex.com>
2019-03-11 18:26:53 -0400

    fix benchmark naming bug

--- benches/plane_bench.rs
+++ benches/plane_bench.rs
@@ -21,18 +21,18 @@ fn bench_make_xy_plane(c: &mut Criterion) {
     );
 }
 
-fn bench_make_yz_plane(c: &mut Criterion) {
+fn bench_make_zx_plane(c: &mut Criterion) {
     let bounds = geo::RectBounds::new(100.0, 100.0);
 
-    c.bench_function("make xz plane",
-        move |b| b.iter(|| geo::xz_plane(&bounds))
+    c.bench_function("make zx plane",
+        move |b| b.iter(|| geo::zx_plane(&bounds))
     );
 }
 
 criterion_group!(benches, bench_memoized_inverse,
                           bench_eager_inverse,
                           bench_make_xy_plane,
-                          bench_make_yz_plane,
+                          bench_make_zx_plane,
                 );
 
 criterion_main!(benches);
